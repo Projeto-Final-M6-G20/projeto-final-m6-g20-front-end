3443d89d82354d51510f25a1a58b73a4432cbcbc
Cielzx <santosjaciel30@gmail.com>
2023-06-09 21:24:13 -0300

    Feat: Homepage add

--- src/app/components/Banner/index.tsx
+++ src/app/components/Banner/index.tsx
@@ -3,17 +3,17 @@ const Banner = () => {
     "https://i.pinimg.com/736x/cc/d8/85/ccd8856d299d01b93af4f66f88069ff1.jpg";
   return (
     <section
-      className="w-full bg-no-repeat bg-cover bg-center"
+      className="w-full md:h-screen bg-no-repeat bg-cover bg-center"
       style={{
         backgroundImage: `url(${bmw})`,
         height: "500px",
       }}
     >
-      <div className="w-full h-full flex flex-col justify-center items-center gap-5 bg-gradient-to-t from-black to-transparent">
-        <h1 className="heading-1-700 text-4xl flex justify-center text-white">
+      <div className="w-full h-full flex flex-col justify-center items-center gap-5 bg-gradient-to-t from-black to-transparent ">
+        <h1 className="heading-1-700 text-4xl flex justify-center text-white relative bottom-9">
           Motors Shop
         </h1>
-        <h2 className="heading-3-500 text-white w-full flex justify-center ">
+        <h2 className="heading-3-500 text-white w-full flex justify-center relative bottom-9 ">
           A melhor plataforma de anuncio de carros do país
         </h2>
       </div>
