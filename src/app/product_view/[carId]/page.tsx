f0ca024fc0265b4da9f023116a618791743e6a8e
lucasbatista-dev <lucas_pivatto@hotmail.com.br>
2023-06-28 09:59:06 -0300

    feat: update layout car view

--- src/app/product_view/[carId]/page.tsx
+++ src/app/product_view/[carId]/page.tsx
@@ -17,12 +17,21 @@ const AdDetailView = ({ params }: { params: { carId: string } }) => {
     return <Spinner color="blue" />;
   }
 
+  const getInitials = (name: string) => {
+    const initials = name
+      .split(' ')
+      .map((word) => word.charAt(0))
+      .join('');
+
+    return initials;
+  };
+
   return (
-    <main className="flex min-h-screen min-w-full items-start justify-center backgroundImage bg-cover bg-no-repeat py-6">
+    <main className="flex min-h-screen min-w-full items-start justify-center backgroundImage bg-cover bg-no-repeat">
       <Container>
-        <section className="flex items-start justify-between w-4/5 ">
-          <section className="flex flex-col w-3/6 justify-center gap-6 ">
-            <div className="bg-white h-60 flex justify-center ">
+        <section className="flex flex-col items-center lg:flex-row lg:items-start justify-between w-full  ">
+          <section className="flex flex-col w-[90%] lg:w-[63%] justify-center gap-6 ">
+            <div className="bg-white h-[380px] flex justify-center ">
               <img
                 className=" w-3/6 object-contain "
                 src={car.images[0].url}
@@ -53,7 +62,7 @@ const AdDetailView = ({ params }: { params: { carId: string } }) => {
             </div>
           </section>
 
-          <section className="flex flex-col w-2/6 justify-center gap-6 ">
+          <section className="flex flex-col w-[90%] lg:w-35% lg:w-2/6 justify-center gap-6">
             <div className="flex flex-col justify-items-center gap-4 bg-white p-8 h-64">
               <h2 className="text-lg font-semibold">Fotos</h2>
               <div className="flex flex-wrap gap-2 gap-y-6">
@@ -66,12 +75,13 @@ const AdDetailView = ({ params }: { params: { carId: string } }) => {
             </div>
 
             <div className="grid justify-items-center gap-6 bg-white p-8">
-              <div className="rounded-full bg-brand-1 w-20 h-20"></div>
-              <h1 className="text-lg font-bold">Samuel Leão</h1>
-              <p className="mb-3 text-gray-500">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industrys
-              </p>
+              <div className="rounded-full bg-brand-1 w-20 h-20">
+                <p className="w-full h-full flex justify-center items-center  text-white text-4xl">
+                  {getInitials(car.User.fullname)}
+                </p>
+              </div>
+              <h1 className="text-lg font-bold">{car.User.fullname}</h1>
+              <p className="mb-3 text-gray-500">{car.User.description}</p>
               <button
                 type="button"
                 className="text-white bg-primary hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5"
